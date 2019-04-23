bd0fe6ee460bd39fe586f8bf2153dd2d5750fe4a
Jacky Hung <jacky5559tw@gmail.com>
2019-04-23 21:14:33 +0800

    Update age.js

--- JS/age.js
+++ JS/age.js
@@ -1,5 +1,15 @@
-$('#Run').on('click', function() {
+var checkage = function() {
     var age = $('#InputAge').val()
     if (age >= 18) $('#Output').val('Adult')
     else $('#Output').val('not Adult')
-})
+}
+
+$('#Run').on('click', checkage)
+
+/*
+    $('#Run').on('click', function() {
+        var age = $('#InputAge').val()
+        if (age >= 18) $('#Output').val('Adult')
+        else $('#Output').val('not Adult')
+    })
+*/
